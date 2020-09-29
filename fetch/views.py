2ad29dfebc911065dea3eef462dae34ff02e65b1
UdPro <abhinav.17.becs@gmail.com>
2020-09-29 12:47:03 +0530

    corr in scracping

--- fetch/views.py
+++ fetch/views.py
@@ -49,6 +49,8 @@ csrf = None
 link1 = None
 butUrl = None
 session = None
+#debug for details
+debug = True
 
 # for local use
 user = 'root'
@@ -255,7 +257,7 @@ def results(request):
 	# removing the Capthca image incase of cache file remain saved
 	# use CTRL + F5 to clear cache
 
-	#remove("G://vtu_result//static//capt.png")
+	remove("G://vtu_result//static//capt.png")
 	captain = str(request.POST['cap'])
 	print(str(request.POST['sec']), str(request.POST['sem']) ,str(request.POST['year']))
 	file = request.POST['csv']
@@ -275,18 +277,24 @@ def results(request):
 	data = {}
 	data[("Name", '')] = {}
 
-	# Sending request
-
-	for usn in tqdm(l,desc = "Scraping",unit = 'data'):
-		# print(usn)
-		payload = {'lns': usn,
+	payload = {'lns': None,
         'captchacode':captain,
         'Token': csrf,
         'current_url': butUrl+'index.php'
         }
+
+	# Sending request
+	captha_check = True
+	for usn in tqdm(l,desc = "Scraping",unit = 'data'):
+
+		payload['lns'] = usn
 		resp = session.post(link1, headers=headers ,data = payload, cookies = cookies ,allow_redirects=False, verify = False)
 		page_soup = soup(resp.text, 'html.parser')
 
+		if captha_check and 'Invalid captcha' in page_soup.script.text.strip():
+			captha_check = False
+			messages.error(request,'Invalid Capthcha ! If problem presist try clearing Cache')
+			return redirect('fetch_result')
 		# Scraping the result table from page
 
 		container = page_soup.findAll("div", {'class':'divTableRow'})
@@ -297,18 +305,19 @@ def results(request):
 			# comment req
 			if 'not available' in page_soup.script.text:
 				print('Invalid Usn for', usn)
-				messages.error(request,'Invalid Capthcha ! If problem presist try clearing Cache')
-				return redirect('fetch_result')
+				continue
 			if 'check website' in page_soup.script.text:
 				print('Got timeout sending Multiple requests')
 				while('check website' in page_soup.script.text):
 					resp = session.post(link1, headers=headers,data = payload, cookies = cookies,allow_redirects=True)
 					page_soup = soup(resp.text, 'html.parser')
 				container = page_soup.findAll("div", {'class':'divTableRow'})
-		print(payload['lns'])
+		
 		nameConainter = page_soup.findAll('td', {'style':'padding-left:15px'})
 		name  = nameConainter[0].text
-		print(name[2:])
+		if debug:
+			print(payload['lns'])
+			print(name[2:])
 		Usn = payload['lns']
 		data[("Name",'')][Usn] = name[2:]
 		for con in range(1,len(container)-1):
@@ -319,31 +328,31 @@ def results(request):
 
 			if marks[0].text == 'Subject Code':
 					break
-			print(marks[0].text ,marks[2].text+ ',' + marks[3].text + ',' +  marks[4].text + ',', marks[5].text )
+			if debug : print(marks[0].text ,marks[2].text+ ',' + marks[3].text + ',' +  marks[4].text + ',', marks[5].text )
 
 			# Try catch if Key is not found in Dict
 
 			try:
-				data[(marks[0].text,"Internal")][Usn] = marks[2].text
+				data[(marks[0].text,"Internal")][Usn] = int(marks[2].text)
 			except(KeyError):
 				data[(marks[0].text,"Internal")] = {}
-				data[(marks[0].text,"Internal")][Usn] = marks[2].text
+				data[(marks[0].text,"Internal")][Usn] = int(marks[2].text)
 				
 			# If External key not found create the External key else insert element in Dict
 
 			try:
-				data[(marks[0].text,"External")][Usn] = marks[3].text
+				data[(marks[0].text,"External")][Usn] = int(marks[3].text)
 			except(KeyError):
 				data[(marks[0].text,"External")] = {}
-				data[(marks[0].text,"External")][Usn] = marks[3].text
+				data[(marks[0].text,"External")][Usn] = int(marks[3].text)
 
 			# If Total key not found in Dict, create the result key and insert element in Dict
 
 			try:
-				data[(marks[0].text,"Total")][Usn] = marks[4].text
+				data[(marks[0].text,"Total")][Usn] = int(marks[4].text)
 			except(KeyError):
 				data[(marks[0].text,"Total")] = {}
-				data[(marks[0].text,"Total")][Usn] = marks[4].text
+				data[(marks[0].text,"Total")][Usn] = int(marks[4].text)
 
 			# If result key not found in Dict, create the result key and insert element in Dict
 
@@ -352,6 +361,7 @@ def results(request):
 			except(KeyError):
 				data[(marks[0].text,"Result")] = {}
 				data[(marks[0].text,"Result")][Usn] = marks[5].text
+		if debug: print()
 	
 	# Casting result into Dataframe
 
@@ -363,6 +373,7 @@ def results(request):
 	z.index.names = ['Usn','Name', 'Subject']
 	z['sec'] = str(request.POST['sec'])
 	sem = str(request.POST['sem'])
+	pass_ia_marks = int(request.POST['passing_ia'])
 	mydb = connect_db()
 	conn = mydb.cursor()
 	database = str(request.POST['year'])
@@ -375,108 +386,7 @@ def results(request):
 	z.to_sql(name=sem, con=conn, if_exists = 'append' ,dtype={'Usn': VARCHAR(10),'Name': VARCHAR(50), 'Subject': VARCHAR(10)}) 
 	#
 	t.index.name = "Usn"
-	subject=list(t.columns.levels[0][:])
-	if 'Name' in subject:
-		subject.remove('Name')
-	for sub in subject:
-		t.loc[:,(sub,['Internal','External','Total'])] = t.loc[:,(sub,['Internal','External','Total'])].astype('float64').astype('Int64')
-	t['Grand Total'] = t.iloc[:,1:].fillna(0).apply(Total,args = [subject],axis = 1)
-	detail = {}
-	for sub in subject:
-		detail[sub] = {}
-	for sub in subject:
-		df = t[sub]['Result'].value_counts()
-		detail[sub] = { 'P': 0,
-					'A': 0,
-					'F': 0,
-					}
-		for x in df.index:
-			detail[sub][x] = df[x]
-		detail[sub]['appeared'] = t[sub]['Result'].count() 
-	for x in detail.keys():
-		for fc in ['FCD','FC','SC']:
-			detail[x][fc] = 0
-	for sub in subject:
-		df = t[sub].apply(grade,axis = 1).value_counts()
-		for x in df.index:
-				detail[sub][x] = df[x]
-	sub_analysis = pd.DataFrame.from_dict(detail,orient = 'index')
-	sub_analysis['pass_%'] = sub_analysis['P']/sub_analysis['appeared']*100
-	sub_analysis['pass_%'] = sub_analysis['pass_%'].apply(lambda x : round(x,2))
-	sub_analysis['Avg Marks(EXT)'] = 0
-	sub_analysis['Avg Marks(INT)'] = 0
-	for sub in subject:
-		sub_analysis.loc[sub ,'Avg Marks(EXT)'] = round(t[sub]['External'].mean(),2)
-		sub_analysis.loc[sub ,'Avg Marks(INT)'] = round(t[sub]['Internal'].mean(),2)
-
-	topper = t[['Name','Grand Total']].reset_index().sort_values('Grand Total', ascending = False).head().stack()
-	topper.set_index('Usn',inplace = True)
-	topper = topper[['Name','Grand Total' ]]
-	pass_ia = {}
-
-	# Passing marks for ia
-	fail_ia_marks = 19
-	for no in range(len(subject)+1):
-		pass_ia['Total Students failed in '+str(no)] = {'Ext' : 0,
-					'Int': 0}
-	def pass_fail(x):
-		ia, ext = 0,0  
-		for sub in subject:
-			if x[sub,'Result'] == 'F':
-				if x[sub,'Internal'] < fail_ia_marks:
-					ia += 1
-				else:
-					ext += 1
-		pass_ia['Total Students failed in '+str(ia + ext)]['Ext'] += ext
-		pass_ia['Total Students failed in '+str(ia + ext)]['Int'] += ia
-	t.apply(pass_fail, axis = 1)
-	del pass_ia['Total Students failed in ' + '0']
-	pass_due_to_ia = pd.DataFrame.from_dict(pass_ia, orient = 'index')
-
-	# Creating dic_class for student detail in a Class
-
-	dic_class = {
-		"STUDENT APPEARED" : 0,
-		"FIRST CLASS DISTIN":  0,
-		"FIRST CLASS" : 0,
-		"SECOUND CLASS" : 0,
-		"PASSED" : 0,
-		"FAILED" : 0,
-		"ABSENT": 0,
-	}
-
-	def student_grade(x,marks):
-		if x*100/marks >= 70:
-			return 'FIRST CLASS DISTIN'
-		elif x*100/marks >= 60:
-			return 'FIRST CLASS'
-		else:
-			return 'SECOUND CLASS'
-
-	def dic_pass(x):
-		dic_class['STUDENT APPEARED'] += 1
-		absent, fail = 0, 0
-		for sub in subject:
-			if x[sub, 'Result'] == 'F':
-				fail += 1
-			elif x[sub, 'Result'] == 'A':
-				absent += 1
-		if fail == 0 and absent == 0:
-			dic_class['PASSED'] += 1
-			dic_class[student_grade(x['Grand Total'][0], total_marks)] += 1
-			
-		else:
-			dic_class['FAILED'] += 1
-		if absent :
-			dic_class['ABSENT'] += 1
-
-	t.apply(dic_pass, axis = 1)
-
-	# Changing the row 
-
-	students_div = pd.DataFrame.from_dict(dic_class, orient = 'index', columns =['Freq'])
-	students_div.loc["Overall Pass"] = int(students_div.loc['PASSED']*100/students_div.loc['STUDENT APPEARED'])
-
+	t, sub_analysis, topper, students_div, pass_due_to_ia = analyze(t, total_marks, pass_ia_marks)
 	colorFile(t,sub_analysis, topper, students_div, pass_due_to_ia)
 
 
@@ -512,7 +422,10 @@ def analyze(t, total_marks = 800, fail_ia_marks = 19):
 	# Problem May arise if number of subjects are changed or differ
 	# Patch will be soon released
 
-	subject=list(t.columns.levels[0][:9])
+	subject=list(t.columns.levels[0][:])
+	if 'Name' in subject:
+		subject.remove('Name')
+	
 
 	# Converting the datatype of result(Internal, External, Total) into int64
 	# Converting nan to float64 and then to int64
